6174884d756a1de24a212156cd179e5f020d2f8c
GrigoryTolmachev <145544412+GrigoryTolmachev@users.noreply.github.com>
2024-02-26 16:20:52 +0700

    Update HalfMage.cpp

--- HalfMage.cpp
+++ HalfMage.cpp
@@ -1,34 +1,28 @@
 #include<iostream>
 #include<string>
 #include "class.cpp"
-#include "magic.cpp"
+#include "Magic.cpp"
 class HalfMage: public Class{
 private:
     int attacks_ =2;
     int magicskill =1;
 public:
-    int ShowAttacks(int lvl){return(attacks_)}
+    int ShowAttacks(){return(attacks_)}
 };
 class Paladin: public HalfMage{
 private:
     std::string main_characteristic = "charisma";
     int DiceHP = 10;
-public:
-    std::string GetMainCharacteristic(){return(main_characteristic);}
 };
 class Ranger: public HalfMage{
 private:
     std::string main_characteristic = "wisdom";
     int DiceHP = 10;
-public:
-    std::string GetMainCharacteristic(){return(main_characteristic);}
 };
 class Artificer: public HalfMage{
 private:
     std::string main_characteristic = "intelligence";
     int DiceHP = 8;
-public:
-    std::string GetMainCharacteristic(){return(main_characteristic);}
 };
 
 
