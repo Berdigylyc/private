be41f7b764b2bc68db7806118e6511bf0ad43979
Your Name <your_email@whatever.com>
2024-02-26 16:52:11 +0300

    commit

--- Mage.cpp
+++ Mage.cpp
@@ -4,22 +4,23 @@
 #include "Magic.cpp"
 
 class Mage_ : virtual public Class_, public Magic{
-private:
+public:
     int attacks_ = 1;
     int magic_skill = 2;
+    Mage_(int a,int b, str r):Class_(1,b,2,r){}
 };
 class Wizard: public Mage_{
-private:
+public:
     std::string main_characteristic = "intelligence";
-    int DiceHP = 6;
+    Wizard(): Mage_(6, "Intelligence"){}
 };
 class Bard: public Mage_{
-private:
+public:
     std::string main_characteristic = "charisma";
-    int DiceHP = 8;
+    Bard(): Mage_(8, "Charisma"){}
 };
 class Druid: public Mage_{
-private:
+public:
     std::string main_characteristic = "Wisdom";
-    int DiceHP = 8;
+    Druid(): Mage_(8, "Wisdom"){}
 };
