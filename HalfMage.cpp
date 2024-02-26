be3eba9efa1b74e56928dd6d78f43da7c8d8bc15
Григорий Толмачев <tolmachev.gm@phystech.edu>
2024-02-26 20:55:02 +0700

    new

--- HalfMage.cpp
+++ HalfMage.cpp
@@ -4,23 +4,31 @@
 #include "Magic.cpp"
 class HalfMage: public Class_, public Magic{
 private:
-    int attacks_ =2;
-    int magic_skill =1;
+    int attacks_;
+    int magic_skill;
+public:
+    HalfMage(int hp, std::string character):Class(2,hp,1,character)
 };
 class Paladin: public HalfMage{
 private:
-    std::string main_characteristic = "charisma";
-    int DiceHP = 10;
+    std::string main_characteristic;
+    int DiceHP;
+public:
+    Paladin():HalfMage(10,"charisma")
 };
 class Ranger: public HalfMage{
 private:
-    std::string main_characteristic = "wisdom";
-    int DiceHP = 10;
+    std::string main_characteristic;
+    int DiceHP;
+pubic:
+    Ranger():HalfMage(10,"wisdom")
 };
 class Artificer: public HalfMage{
 private:
-    std::string main_characteristic = "intelligence";
-    int DiceHP = 8;
+    std::string main_characteristic;
+    int DiceHP;
+public:
+    Artificer():HalfMage(8,"intelligence")
 };
 
 
