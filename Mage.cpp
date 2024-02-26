56ca9f7e5a126f84318d61488ab075a9f6fb35f9
GrigoryTolmachev <145544412+GrigoryTolmachev@users.noreply.github.com>
2024-02-26 17:17:06 +0700

    Update Mage.cpp

--- Mage.cpp
+++ Mage.cpp
@@ -5,33 +5,23 @@
 
 class Mage_ : public Class_, public Magic{
 private:
-    int attack_ = 1;
+    int attacks_ = 1;
     int magic_skill = 2;
 public:
-    int GetAttack(){return (attack_);}
-    int GetMagicSkill() {return(magicskill);}
+    int showattacks(){return (attacks_);}
 };
 class Wizard: public Mage_{
 private:
     std::string main_characteristic = "intelligence";
     int DiceHP = 6;
-public:
-    std::string GetMainCharacteristic(){return(main_characteristic);}
-    int GetDiceHP(){return(DiceHP);}
 };
 class Bard: public Mage_{
 private:
     std::string main_characteristic = "charisma";
     int DiceHP = 8;
-public:
-    std::string GetMainCharacteristic(){return(main_characteristic);}
-    int GetDiceHP(){return(DiceHP);}
 };
 class Druid: public Mage_{
 private:
     std::string main_characteristic = "Wisdom";
     int DiceHP = 8;
-public:
-    std::string GetMainCharacteristic(){return(main_characteristic);}
-    int GetDiceHP(){return(DiceHP);}
 };
